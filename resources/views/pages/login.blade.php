5ba6efbb56feb243db8fccb481df1f70184abe05
David Kurniawan <david.kurniawan4331@gmail.com>
2024-09-21 13:10:15 +0700

    coba login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,43 +1,136 @@
-@include('layouts.header')
-<body>
-    <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
+<!DOCTYPE html>
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="{{asset('sneat')}}/assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
 
-        {{-- menu --}}
-        @include('layouts.sidebar')
+    <title>Dashboard - Cek Kelulusan Mahasiswa</title>
 
-        <!-- Layout container -->
-        <div class="layout-page">
+    <meta name="description" content="" />
 
-            {{-- navbar --}}
-            @include('layouts.navbar')
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{asset('sneat')}}/assets/img/favicon/favicon.ico" />
 
-                <!-- Content -->
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
 
-                <h4 class="fw-bold p-4">Blank Page</h4>
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="{{asset('sneat')}}/assets/vendor/fonts/boxicons.css" />
 
-                <!-- / Content -->
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{asset('sneat')}}/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{asset('sneat')}}/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{asset('sneat')}}/assets/css/demo.css" />
 
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{asset('sneat')}}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-                <!-- Core JS -->
-                <!-- build:js assets/vendor/js/core.js -->
-                <script src="{{asset('sneat')}}/assets/vendor/libs/jquery/jquery.js"></script>
-                <script src="{{asset('sneat')}}/assets/vendor/libs/popper/popper.js"></script>
-                <script src="{{asset('sneat')}}/assets/vendor/js/bootstrap.js"></script>
-                <script src="{{asset('sneat')}}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{asset('sneat')}}/assets/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+    <script src="{{asset('sneat')}}/assets/vendor/js/helpers.js"></script>
 
-                <script src="{{asset('sneat')}}/assets/vendor/js/menu.js"></script>
-                <!-- endbuild -->
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{asset('sneat')}}/assets/js/config.js"></script>
+  </head>
 
-                <!-- Vendors JS -->
+  <body>
+    <!-- Content -->
 
-                <!-- Main JS -->
-                <script src="{{asset('sneat')}}/assets/js/main.js"></script>
+    <div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Register -->
+          <div class="card">
+            <div class="card-body">
+              <!-- Logo -->
+              <div class="app-brand justify-content-center">
+                <a  class="app-brand-link gap-2">
+                  <span class="app-brand-logo demo">
+                    <img
+                    width="35"
+                    src="{{asset('sneat')}}/assets/img/logo.png">
+                  </span>
+                  <span class="app-brand-text demo text-body fw-bolder">Vokasi UB</span>
+                </a>
+              </div>
+              <!-- /Logo -->
+              <h4 class="mb-2">Prediksi Kelulusan Mahasiswa</h4>
+              <p class="mb-4">Fakultas Vokasi Universitas Brawijaya</p>
 
-                <!-- Page JS -->
+              <form id="formAuthentication" class="mb-3" >
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email or Username</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="email"
+                    name="email-username"
+                    placeholder="Enter your email or username"
+                    autofocus
+                  />
+                </div>
+                <div class="mb-3 form-password-toggle">
+                  <div class="d-flex justify-content-between">
+                    <label class="form-label" for="password">Password</label>
+                  </div>
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      aria-describedby="password"
+                    />
+                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                  </div>
+                </div>
+                  <button  class="btn btn-primary d-grid w-100" type="submit">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-                <!-- Place this tag in your head or just before your close body tag. -->
-                <script async defer src="https://buttons.github.io/buttons.js"></script>
-    </body>
+    <!-- / Content -->
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="{{asset('sneat')}}/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="{{asset('sneat')}}/assets/vendor/libs/popper/popper.js"></script>
+    <script src="{{asset('sneat')}}/assets/vendor/js/bootstrap.js"></script>
+    <script src="{{asset('sneat')}}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="{{asset('sneat')}}/assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="{{asset('sneat')}}/assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
 </html>
